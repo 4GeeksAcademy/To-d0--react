876d2eb533150f6fd76c829b968f48959d5a8793
DianisMartinez <dianagomez192002@gmail.com>
2024-11-10 21:18:03 +0000

    hola

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [item, setItem] = useState(""); 
+    const [tasks, setTask] = useState([ 
+        "Brush my teeth",
+        "Prepare breakfast",
+        "Study React basics",
+        "Read a chapter of a book",
+        "6 items remaining"
+    ]);
+
+ //agregar una tarea a la lista
+    const addTask = (e) => {
+        if (e.key === "Enter" && item.trim() !== "") {
+            setTask([...tasks, item]); 
+            setItem(""); 
+        }
+    };
+
+   // eliminar una tarea ala lissta
+    const deleteTask = (index) => {
+        const newTasks = tasks.filter((task, i) => i !== index); 
+        setTask(newTasks); 
+	} 
+    return (
+        <div className="container">
+            <h1>Things to Do</h1>  
+            <div>
+                <ul>
+                    <li>
+                        <input
+                            type="text" 
+                            placeholder="Add a task" 
+                            value={item}
+                            onChange={(e) => setItem(e.target.value)} 
+                            onKeyDown={addTask} 
+                        />
+                    </li>
+                    {tasks.map((task, index) => (
+                        <li key={index}>
+                            {task} 
+                            <i className="fas fa-times" onClick={() => deleteTask(index)}></i> {}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
